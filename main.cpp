c773a0a125d28b0511a57513102d792cd12a9434
Alejandro Zuniga <alejandro927>
2021-05-07 15:01:00 -0600

    el 2 esta completo

--- main.cpp
+++ main.cpp
@@ -17,16 +17,142 @@ int menu() {
 	return opcion;
 }
 
-void Ejercicio1(){
-	
+void ImprimirArr(char* arreglo, int size) {
+	for(int i=0; i<size; i++) {
+		cout<<arreglo[i]<<" ";
+	}
 }
 
-void Ejercicio2(){
-	
+void Ejercicio1() {
+	int size=0;
+	cout<<"Ingrese el tamano del arreglo:";
+	cin>>size;
+
+	char* arreglo = new char[size];
+
+	int repeticiones [size/2];
+	char caracteres [size/2];
+
+	int contadorCaracter=0;
+	int contadorRepeticiones=0;
+
+
+	for(int i=0; i<size; i++) {
+		cout<<"Ingrese el caracter:";
+		cin>> arreglo[i];
+	}
+
+	for(int i=0; i<size; i++) {
+		if(i%2 == 0) {
+			int val = arreglo[i] - 48;
+			repeticiones[contadorRepeticiones] = val;
+			contadorRepeticiones++;
+		} else {
+			caracteres[contadorCaracter] = arreglo[i];
+			contadorCaracter++;
+		}
+	}
+	ImprimirArr(arreglo,size);
+
+
+	cout<<endl<<endl;
+	for(int i=0; i<size/2; i++) {
+		int rep = 0;
+		for(int j=0 ; j < repeticiones[i] ; j++) {
+			cout<<" "<<caracteres[i];
+		}
+	}
+
+	delete []arreglo;
 }
 
-void Ejercicio3(){
-	
+void LlenarMat(char** matrix, int sizeA,int sizeB) {
+	for(int i = 0; i<sizeA; i++) {
+		for(int j=0; j<sizeB; j++) {
+			matrix[i][j] ='-';
+		}
+	}
+
+}
+
+void ImprimirMat(char** matrix, int sizeA,int sizeB) {
+	for(int i = 0; i<sizeA; i++) {
+		cout<<"| ";
+		for(int j=0; j<sizeB; j++) {
+			cout<<"'"<<matrix[i][j]<<"' ";
+		}
+		cout<<"|";
+		cout<<endl;
+	}
+}
+
+char** Obstaculos(char** matriz,int obstaculos,int filas, int columnas) {
+	int fil[1000];
+	int col[1000];
+	for(int i=0; i < obstaculos; i++) {
+		int ran1 = rand() % filas;
+		int ran2 = rand() % columnas;
+		for(int i=0; i<1000; i++) {
+			while((ran1 == fil[i]) && (ran2 == col[i])) {
+				ran1 = rand() % filas;
+				ran2 = rand() % columnas;
+			}
+			fil[i]=ran1;
+			col[i] = ran2;
+		}
+
+		char m = matriz[ran1][ran2];
+		int letra = '-';
+
+		if(matriz[ran1][ran2]==letra) {
+			matriz[ran1][ran2]='#';
+		}
+	}
+	return matriz;
+}
+
+void Ejercicio2() {
+	int filas,columnas;
+	cout<<"Ingrese las filas:";
+	cin>>filas;
+	cout<<"Ingrese las columnas:";
+	cin>>columnas;
+	cout<<"Ingrese la cantidad de obstaculos:";
+	int obstaculos=0;
+	cin>>obstaculos;
+	char** matriz = 0;
+	matriz=new char*[filas];
+	for(int i=0; i<filas; i++) {
+		matriz[i] = new char[columnas];
+	}
+
+	LlenarMat(matriz,filas,columnas);
+	ImprimirMat(matriz,filas,columnas);
+	Obstaculos(matriz,obstaculos,filas,columnas);
+	cout<<"Matriz final"<<endl;
+	ImprimirMat(matriz,filas,columnas);
+//limpiar memoria
+	for(int i = 0; i<filas; i++) {
+		if(matriz[i]) {
+			delete[] matriz[i];
+			matriz[i] = 0; //asignar null
+		}
+
+	}
+
+	if( matriz != NULL ) {
+		delete[] matriz;
+		matriz = 0;
+	}
+
+	if(matriz) {
+		delete[] matriz;
+		matriz = 0;
+	}
+}
+
+void Ejercicio3() {
+
 }
 
 int main(int argc, char** argv) {
